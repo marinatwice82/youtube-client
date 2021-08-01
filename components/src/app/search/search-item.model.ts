f940cfbaa759aafccc794e4ed99ecef5ac3f8c68
Marina <marinatwice82@gmail.com>
2021-08-01 16:53:35 +0300

    add models

---
+++ components/src/app/search/search-item.model.ts
@@ -0,0 +1,12 @@
+export interface SearchItem {
+    kind: string;
+    etag: string;
+    id: string;
+    title: string;
+    img: string;
+    viewCount: string;
+    likeCount: string;
+    dislikeCount: string;
+    commentCount: string;
+
+}
